61d40f47934a2d3ae175bddb69a48fca825f3535
Michael Keselman <michael@soundraw.co.jp>
2025-01-14 21:28:25 +0800

    update styles

--- app/questions/ui/game.tsx
+++ app/questions/ui/game.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { useSwipeable } from "react-swipeable";
 import { Button } from "@telegram-apps/telegram-ui";
-import { QUESTIONS } from "../constants/questions";
+import { QUESTIONS, questionsCategoriesLabels } from "../constants/questions";
 
 interface GameProps {
   onEndGame: () => void;
 }
 
 export default function Game({ onEndGame }: GameProps) {
+  const randomQuestions = useMemo(
+    () => QUESTIONS.sort(() => Math.random() - 0.5),
+    []
+  );
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handlers = useSwipeable({
@@ -34,10 +38,13 @@ export default function Game({ onEndGame }: GameProps) {
           className="cards-wrapper"
           style={{ transform: `translateX(-${currentIndex * 100}%)` }}
         >
-          {QUESTIONS.map((card) => (
+          {randomQuestions.map((card) => (
             <div key={card.id} className="card">
               <div className={`card-content ${card.type}`}>
-                {card.text}
+                <div className="card-text">{card.text}</div>
+                <div className="card-category">
+                  {questionsCategoriesLabels[card.type]}
+                </div>
               </div>
             </div>
           ))}
