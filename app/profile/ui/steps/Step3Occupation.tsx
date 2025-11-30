41abb219997a6b59ca3bbb2309a503e4c22582e4
Mikhail Keselman <dasdgool@gmail.com>
2025-11-30 20:25:22 +0800

    up

--- app/profile/ui/steps/Step3Occupation.tsx
+++ app/profile/ui/steps/Step3Occupation.tsx
@@ -1,5 +1,5 @@
+//@ts-nocheck
 "use client";
-
 import { useTranslations } from "next-intl";
 import { Input, StepContainer } from "@/components";
 import { StepProps } from "@/models/types";
@@ -8,15 +8,19 @@ import { useWizardContext } from "../WizardContext";
 export interface Step3OccupationProps extends StepProps {}
 
 export function Step3Occupation({ onNext }: Step3OccupationProps) {
-  const t = useTranslations('profile.steps.occupation');
-  const { register, watch, formState: { errors } } = useWizardContext();
-  
+  const t = useTranslations("profile.steps.occupation");
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useWizardContext();
+
   const occupation = watch("occupation") || "";
   const isValidOccupation = occupation.trim().length >= 2;
 
   return (
     <StepContainer
-      title={t('title')}
+      title={t("title")}
       onNext={onNext}
       nextDisabled={!isValidOccupation}
     >
@@ -25,17 +29,17 @@ export function Step3Occupation({ onNext }: Step3OccupationProps) {
           required: true,
           minLength: {
             value: 2,
-            message: t('error')
-          }
+            message: t("error"),
+          },
         })}
         type="text"
-        placeholder={t('placeholder')}
+        placeholder={t("placeholder")}
         maxLength={50}
         required
       />
       {errors.occupation && (
         <div className="input-error-text">
-          {errors.occupation.message || t('error')}
+          {errors.occupation.message || t("error")}
         </div>
       )}
     </StepContainer>
